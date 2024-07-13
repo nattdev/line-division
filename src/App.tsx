92938fee6415242af8be5870a81447a8d4ee3207
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-07-13 01:50:47 -0500

    Create canvas context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [canvasContext, setCanvasContext] = useState<CanvasRenderingContext2D | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+
+      canvas.width = windowWidth;
+      canvas.height = windowHeight;
+
+      const context = canvas?.getContext("2d") || null;
+      setCanvasContext(context);
+    }
+  }, [canvasRef]);
+
   return (
-    <>
-    </>
+    <div>
+      <canvas ref={canvasRef}>
+      </canvas>
+    </div>
   )
 }
 
-export default App
+export default App;
